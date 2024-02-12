c381ad4df0f0b8ad000d9f66ab748819bad9bb7d
Grant Peterson <dgrantpete@gmail.com>
2024-02-12 16:57:19 -0700

    Fixed files that were previously accidentally restored

--- Program.cs
+++ Program.cs
@@ -24,10 +24,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
--- Services/IStoryGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using LinguacApi.Data;
-
-namespace LinguacApi.Services
-{
-    public interface IStoryGenerator
-    {
-        Task<string> GenerateStoryContent(Language language, CefrLevel level);
-    }
-}
