165ccd265566bf9f336abdc7355d4f7ad4a4342a
Grant Peterson <dgrantpete@gmail.com>
2024-03-22 00:53:00 -0600

    Fix namespaces.

--- Services/Email/Confirmation/EmailConfirmer.cs
+++ Services/Email/Confirmation/EmailConfirmer.cs
@@ -1,7 +1,7 @@
 ﻿using Flurl;
 using LinguacApi.Configurations;
 using LinguacApi.Services.Email.EmailGenerator;
-using LinguacApi.Templates.Emails.ConfirmEmail;
+using LinguacApi.Services.Email.Templates.ConfirmEmail;
 using Microsoft.Extensions.Options;
 using SendGrid;
 using SendGrid.Helpers.Mail;
